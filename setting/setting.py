6af0d87949ff9f65f93c18e7d6cfed5c9638583e
vFeng <z1914007838@outlook.com>
2019-01-04 19:40:58 +0800

    Initial commit

---
+++ setting/setting.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+__author__ = 'zhoujifeng'
+__date__ = '2019/1/4 11:09'
+
+OCR_API = {
+    'baidu': {
+        'ocr_url': 'https://aip.baidubce.com/rest/2.0/ocr/v1/general_basic',
+        'token_url': 'https://aip.baidubce.com/oauth/2.0/token',
+        'data': {
+            'appid': 15191630,
+            'client_id': '6N3ZkEujGxlGGU2sOhM1CZug',
+            'client_secret': 'EkctmyzWn2MOw6o1HbBVosxRINUYsTtm',
+            'grant_type': 'client_credentials'
+        },
+
+    }
+}
